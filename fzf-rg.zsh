9a7594ccb3b259fdfc279ffac6ddc1903bb2f6be
shogetoshi <188074504+shogetoshi@users.noreply.github.com>
2025-12-26 18:21:37 +0900

    適切なディレクトリが使われるように

--- fzf-rg.zsh
+++ fzf-rg.zsh
@@ -46,30 +46,34 @@ function get_rbuf() {
     echo "${1## }"
 }
 
-function get_search_path_opt() {
-    local search_path=$(git rev-parse --show-cdup 2>/dev/null)
-    if [[ -n $search_path ]]; then
-        echo "--search-path ${search_path}"
-    else
-        echo ""
-    fi
-}
-
 function fzf-rg() {
     local dir=$(get_dir "${LBUFFER}" "${RBUFFER}")
     local query=$(get_query "${LBUFFER}")
     local lbuf=$(get_lbuf ${LBUFFER})
     local rbuf=$(get_rbuf ${RBUFFER})
-    local out=$(rg -uuu --ignore-file ~/.ignore --color always -n ^ ${dir} 2>/dev/null | \
-        fzf --ansi --multi --reverse --wrap \
-        --query "$query" \
-        --delimiter ':' \
-        --nth '3..' \
-        --preview 'bat --plain --number --color always --highlight-line={2} {1}' \
-        --preview-window '+{2}-5:down:70%' | \
-        awk -F ':' '{print $1}' | \
-        awk '!a[$0]++' \
-    )
+    if [[ -n "$dir" ]]; then
+        local out=$(rg -uuu --ignore-file ~/.ignore --color always -n ^ ${dir} 2>/dev/null | \
+            fzf --ansi --multi --reverse --wrap \
+            --query "$query" \
+            --delimiter ':' \
+            --nth '3..' \
+            --preview 'bat --plain --number --color always --highlight-line={2} {1}' \
+            --preview-window '+{2}-5:down:70%' | \
+            awk -F ':' '{print $1}' | \
+            awk '!a[$0]++' \
+        )
+    else
+        local out=$(rg -uuu --ignore-file ~/.ignore --color always -n ^ $(git rev-parse --show-cdup 2>/dev/null) 2>/dev/null | \
+            fzf --ansi --multi --reverse --wrap \
+            --query "$query" \
+            --delimiter ':' \
+            --nth '3..' \
+            --preview 'bat --plain --number --color always --highlight-line={2} {1}' \
+            --preview-window '+{2}-5:down:70%' | \
+            awk -F ':' '{print $1}' | \
+            awk '!a[$0]++' \
+        )
+    fi
     if [[ -n "$out" ]]; then
         out=$(echo "$out" | sed "s/.*/'&'/" | tr '\n' ' ')
         BUFFER="${lbuf}${out}${rbuf}"
